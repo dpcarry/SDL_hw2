4886bf06e493bd9b6f6d7d2d12805b76d7197903
dpcarry <pd2170@nyu.edu>
2024-10-12 17:33:22 -0400

    hw2

--- main.cpp
+++ main.cpp
@@ -1,28 +1,40 @@
+/**
+* Author: Pingchuan Dong
+* Assignment: Pong Clone
+* Date due: 2024-10-12, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define LOG(argument) std::cout << argument << '\n'
-#define GL_GLEXT_PROTOTYPES 1
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
 #endif
 
+#define GL_GLEXT_PROTOTYPES 1
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
 #include "stb_image.h"
+#include "cmath"
+#include <ctime>
+#include <cstdlib>
 
 enum AppStatus { RUNNING, TERMINATED };
 
-constexpr int WINDOW_WIDTH = 640 * 2,
-WINDOW_HEIGHT = 480 * 2;
+constexpr float WINDOW_SIZE_MULT = 2.0f;
 
-constexpr float BG_RED = 0.9765625f,
-BG_GREEN = 0.97265625f,
-BG_BLUE = 0.9609375f,
-BG_OPACITY = 1.0f;
+constexpr int WINDOW_WIDTH = 640 * WINDOW_SIZE_MULT,
+WINDOW_HEIGHT = 480 * WINDOW_SIZE_MULT;
+
 
 constexpr int VIEWPORT_X = 0,
 VIEWPORT_Y = 0,
@@ -32,55 +44,66 @@ VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 constexpr char V_SHADER_PATH[] = "shaders/vertex_textured.glsl",
 F_SHADER_PATH[] = "shaders/fragment_textured.glsl";
 
+constexpr GLint NUMBER_OF_TEXTURES = 1;
+constexpr GLint LEVEL_OF_DETAIL = 0;
+constexpr GLint TEXTURE_BORDER = 0;
+
 constexpr float MILLISECONDS_IN_SECOND = 1000.0;
 
-constexpr GLint NUMBER_OF_TEXTURES = 1, // to be generated, that is
-LEVEL_OF_DETAIL = 0, // mipmap reduction image level
-TEXTURE_BORDER = 0; // this value MUST be zero
+constexpr char PADDLE_SPRITE_FILEPATH[] = "paddle.png",
+BALL_SPRITE_FILEPATH[] = "ball.png",
+MIDLINE_SPRITE_FILEPATH[] = "mid_line.png";
 
-// source: https://kiminoiro.jp/
-constexpr char RYU_FILEPATH[] = "ryu.png",
-KEN_FILEPATH[] = "ken.png";
 
-constexpr glm::vec3 INIT_SCALE_RYU = glm::vec3(2.56f, 2.06f, 0.0f),
-INIT_SCALE_KEN = glm::vec3(1.90f, 3.65f, 0.0f),
-INIT_POS_RYU = glm::vec3(0.0f, 0.0f, 0.0f),
-INIT_POS_KEN = glm::vec3(-2.0f, 0.0f, 0.0f);
 
-constexpr float BASE_SCALE = 1.7f,      // The unscaled size of your object
-MAX_AMPLITUDE = 0.3f,  // The most our triangle will be scaled up/down
-PULSE_SPEED = 10.0f;
-
-constexpr float ROT_INCREMENT = 1.0f,
-RADIUS = 2.0f,     // radius of your circle
-ORBIT_SPEED = 1.0f;  // rotational speed
-float g_angle = 0.0f,     // current angle
-g_x_offset = 0.0f, // current x and y coordinates
-g_y_offset = 0.0f;
+constexpr glm::vec3 INIT_SCALE_BALL = glm::vec3(0.25f, 0.25f, 0.0f),
+INIT_POS_BALL= glm::vec3(0.0f, 1.0f, 0.0f),
+INIT_SCALE_PADDLE = glm::vec3(0.2f, 1.325f, 0.0f),
+INIT_POS_PADDLE1 = glm::vec3(-4.5f, 0.0f, 0.0f),
+INIT_POS_PADDLE2 = glm::vec3(4.5f, 0.0f, 0.0f),
+INIT_SCALE_MIDLINE = glm::vec3(0.2f, 10.5f, 0.0f),
+INIT_POS_MIDLINE = glm::vec3(0.0f, 0.0f, 0.0f);
 
 SDL_Window* g_display_window;
+
 AppStatus g_app_status = RUNNING;
 ShaderProgram g_shader_program = ShaderProgram();
-bool g_is_growing = true;
-int  g_frame_counter = 0;
+glm::mat4 g_view_matrix, g_paddle1_matrix, g_paddle2_matrix, g_projection_matrix, g_ball_matrix, g_midline_matrix;
 
-glm::mat4 g_view_matrix,
-g_ryu_matrix,
-g_ken_matrix,
-g_projection_matrix;
+float g_previous_ticks = 0.0f;
 
-float g_previous_ticks = 0.0f,
-g_pulse_time = 0.0f;
-
-glm::vec3 g_rotation_ryu = glm::vec3(0.0f, 0.0f, 0.0f),
-g_rotation_ken = glm::vec3(0.0f, 0.0f, 0.0f),
-g_translation_ryu = glm::vec3(0.0f, 0.0f, 0.0f),
-g_translation_ken = glm::vec3(0.0f, 0.0f, 0.0f);
+GLuint g_paddle1_texture_id;
+GLuint g_paddle2_texture_id;
+GLuint g_ball_texture_id;
+GLuint g_midline_texture_id;
 
 
-GLuint g_ryu_texture_id,
-g_ken_texture_id;
+glm::vec3 g_paddle1_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_paddle1_movement = glm::vec3(0.0f, 0.0f, 0.0f);
 
+glm::vec3 g_paddle2_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_paddle2_movement = glm::vec3(0.0f, 0.0f, 0.0f);
+
+glm::vec3 g_ball_position = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_ball_movement = glm::vec3(-1.0f, 0.4f, 0.0f);
+
+
+float ball_speed = 2.8f;
+float paddle_speed = 4.0f;
+bool singlePlayer = false;
+bool gameOver = false;
+
+float distance_1x = 0.0f;
+float distance_1y = 0.0f;
+float distance_2x = 0.0f;
+float distance_2y = 0.0f;
+
+
+void initialise();
+void process_input();
+void update();
+void render();
+void shutdown();
 
 GLuint load_texture(const char* filepath)
 {
@@ -110,13 +133,10 @@ GLuint load_texture(const char* filepath)
     return textureID;
 }
 
-
 void initialise()
 {
-    // Initialise video and joystick subsystems
     SDL_Init(SDL_INIT_VIDEO);
-
-    g_display_window = SDL_CreateWindow("Street Fighter I(but even worse)",
+    g_display_window = SDL_CreateWindow("PONG GAME",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         WINDOW_WIDTH, WINDOW_HEIGHT,
         SDL_WINDOW_OPENGL);
@@ -124,12 +144,8 @@ void initialise()
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
 
-    if (g_display_window == nullptr)
-    {
-        std::cerr << "Error: SDL window could not be created.\n";
-        SDL_Quit();
-        exit(1);
-    }
+
+    if (g_display_window == nullptr) shutdown();
 
 #ifdef _WINDOWS
     glewInit();
@@ -139,8 +155,13 @@ void initialise()
 
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
 
-    g_ryu_matrix = glm::mat4(1.0f);
-    g_ken_matrix = glm::mat4(1.0f);
+    g_paddle1_matrix = glm::mat4(1.0f);
+    g_paddle2_matrix = glm::mat4(1.0f);
+    g_ball_matrix = glm::mat4(1.0f);
+    g_midline_matrix = glm::mat4(1.0f);
+    g_midline_matrix = glm::translate(g_midline_matrix, glm::vec3(0.0f, 0.0f, 0.0f));
+    g_ball_matrix = glm::translate(g_ball_matrix, glm::vec3(1.0f, 1.0f, 0.0f));
+
     g_view_matrix = glm::mat4(1.0f);
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
 
@@ -149,102 +170,218 @@ void initialise()
 
     glUseProgram(g_shader_program.get_program_id());
 
-    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-    g_ryu_texture_id = load_texture(RYU_FILEPATH);
-    g_ken_texture_id = load_texture(KEN_FILEPATH);
+    g_paddle1_texture_id = load_texture(PADDLE_SPRITE_FILEPATH);
+    g_paddle2_texture_id = load_texture(PADDLE_SPRITE_FILEPATH);
+    g_ball_texture_id = load_texture(BALL_SPRITE_FILEPATH);
+    g_midline_texture_id = load_texture(MIDLINE_SPRITE_FILEPATH);
 
+    // enable blending
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
-
 
 void process_input()
 {
     SDL_Event event;
     while (SDL_PollEvent(&event))
     {
-        if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE)
+        switch (event.type)
         {
+            // End game
+        case SDL_QUIT:
+        case SDL_WINDOWEVENT_CLOSE:
             g_app_status = TERMINATED;
+            break;
+
+        case SDL_KEYDOWN:
+            switch (event.key.keysym.sym)
+            {
+            case SDLK_q: g_app_status = TERMINATED; break;
+            case SDLK_t:  singlePlayer = true; break;
+            default: break;
+            }
+
+        default:
+            break;
         }
     }
+
+    const Uint8* key_state = SDL_GetKeyboardState(NULL);
+    g_paddle1_movement = glm::vec3(0.0f);
+    g_paddle2_movement = glm::vec3(0.0f);
+    if (key_state[SDL_SCANCODE_W]) {
+        g_paddle1_movement.y = 1.0f;
+    }
+
+    if (key_state[SDL_SCANCODE_S]) {
+        g_paddle1_movement.y = -1.0f;
+    }
+    if (not(singlePlayer)) {
+        if (key_state[SDL_SCANCODE_DOWN]) {
+            g_paddle2_movement.y = -1.0f;
+        }
+        if (key_state[SDL_SCANCODE_UP]) {
+            g_paddle2_movement.y = 1.0f;
+        }
+    }
+    else {
+            // paddle 2 moving up and down
+        static bool moving_up = true;
+
+            //currently moving up
+        if (moving_up) {
+            g_paddle2_movement.y = 1;
+            if (g_paddle2_position.y > 3.75f - INIT_SCALE_PADDLE.y / 2) {   //border detection
+                moving_up = false;
+            }
+        }
+            //currently going down
+        else {
+            g_paddle2_movement.y = -1;
+            if (g_paddle2_position.y < -3.75f + INIT_SCALE_PADDLE.y / 2) {
+                moving_up = true;
+            }
+        }
+    }
+
+
 }
 
 
 void update()
 {
-    /* Delta time calculations */
+    // --- DELTA TIME CALCULATIONS --- //
     float ticks = (float)SDL_GetTicks() / MILLISECONDS_IN_SECOND;
     float delta_time = ticks - g_previous_ticks;
     g_previous_ticks = ticks;
 
-    /* Game logic */
-    g_pulse_time += delta_time * PULSE_SPEED;
-    float scale_factor = BASE_SCALE + MAX_AMPLITUDE * glm::sin(g_pulse_time); // Use sine for a smoother pulse
-    g_rotation_ryu.y += ROT_INCREMENT * delta_time;
-    g_rotation_ken.y += -1 * ROT_INCREMENT * delta_time;
+    // --- ACCUMULATOR LOGIC --- //
+    g_paddle1_position += g_paddle1_movement * paddle_speed * delta_time;
+    g_paddle2_position += g_paddle2_movement * paddle_speed * delta_time;
+    g_ball_position += g_ball_movement * ball_speed * delta_time;
 
-    /* Model matrix reset */
-    g_ryu_matrix = glm::mat4(1.0f);
-    g_ken_matrix = glm::mat4(1.0f);
+    // --- COLLISION LOGIC --- //
+    float ball_left = g_ball_position.x + INIT_POS_BALL.x - INIT_SCALE_BALL.x / 2;
+    float ball_right = g_ball_position.x + INIT_POS_BALL.x + INIT_SCALE_BALL.x / 2;
+    float ball_top = g_ball_position.y + INIT_POS_BALL.y + INIT_SCALE_BALL.y / 2;
+    float ball_bot = g_ball_position.y + INIT_POS_BALL.y - INIT_SCALE_BALL.y / 2;
 
-    /* Transformations */
-    g_angle += ORBIT_SPEED * delta_time;
-    g_x_offset = RADIUS * glm::cos(g_angle);
-    g_y_offset = RADIUS * glm::sin(g_angle);
-    g_ryu_matrix = glm::translate(g_ryu_matrix, INIT_POS_RYU);
-    g_ryu_matrix = glm::rotate(g_ryu_matrix,
-        g_rotation_ryu.y,
-        glm::vec3(0.0f, 1.0f, 0.0f));
-    g_ryu_matrix = glm::scale(g_ryu_matrix, INIT_SCALE_RYU);
+    // Paddle 1 boundaries
+    float paddle1_right = INIT_POS_PADDLE1.x + INIT_SCALE_PADDLE.x / 2;
+    float paddle1_top = g_paddle1_position.y + INIT_SCALE_PADDLE.y / 2;
+    float paddle1_bot = g_paddle1_position.y - INIT_SCALE_PADDLE.y / 2;
+
+    // Paddle 2 boundaries
+    float paddle2_left = INIT_POS_PADDLE2.x - INIT_SCALE_PADDLE.x / 2;
+    float paddle2_top = g_paddle2_position.y + INIT_SCALE_PADDLE.y / 2;
+    float paddle2_bot = g_paddle2_position.y - INIT_SCALE_PADDLE.y / 2;
+
+//ball collision with paddle1
+    if (ball_left <= paddle1_right && ball_top >= paddle1_bot && ball_bot <= paddle1_top) {
+        float overlap_1x = paddle1_right - ball_left;
+        float overlap_1y = fmin(paddle1_top - ball_bot, ball_top - paddle1_bot);
+
+        //if there's an x-axis overlap, reverse the ball's x velocity and adjust its position to avoid clipping
+        if (overlap_1x > 0) {
+            g_ball_movement.x = -g_ball_movement.x;
+            g_ball_position.x += overlap_1x;
+        }
+    }
+
+    //ball collision with paddle2
+    if (ball_right >= paddle2_left && ball_top >= paddle2_bot && ball_bot <= paddle2_top) {
+        float overlap_2x = ball_right - paddle2_left;
+        float overlap_2y = fmin(paddle2_top - ball_bot, ball_top - paddle2_bot);
+
+        if (overlap_2x > 0) {
+            g_ball_movement.x = -g_ball_movement.x;
+            g_ball_position.x -= overlap_2x;
+        }
+    }
 
 
-    g_ken_matrix = glm::translate(g_ken_matrix, glm::vec3(g_x_offset, g_y_offset, 0.0f));
-    g_ken_matrix = glm::rotate(g_ken_matrix,
-        g_rotation_ken.y,
-        glm::vec3(0.0f, 1.0f, 0.0f));
-    g_ken_matrix = glm::scale(g_ken_matrix, glm::vec3(scale_factor, scale_factor, 1.0f));
+    if (g_paddle1_position.y + INIT_SCALE_PADDLE.y / 2 > 3.75) {
+        g_paddle1_position.y = 3.75 - (INIT_SCALE_PADDLE.y / 2);
+    }
+    if (g_paddle1_position.y - INIT_SCALE_PADDLE.y / 2 < -3.75) {
+        g_paddle1_position.y = -3.75 + (INIT_SCALE_PADDLE.y / 2);
+    }
+    if (not singlePlayer) {
+        if (g_paddle2_position.y + INIT_SCALE_PADDLE.y / 2 > 3.75) {
+            g_paddle2_position.y = 3.75 - (INIT_SCALE_PADDLE.y / 2);
+        }
+        if (g_paddle2_position.y - INIT_SCALE_PADDLE.y / 2 < -3.75) {
+            g_paddle2_position.y = -3.75 + (INIT_SCALE_PADDLE.y / 2);
+        }
+    }
+
+    if (g_ball_position.y > 2.65 || g_ball_position.y < -4.65f) {
+        g_ball_movement.y = -g_ball_movement.y;
+    }
+
+
+
+    // --- TRANSLATION --- //
+    g_paddle1_matrix = glm::mat4(1.0f);
+    g_paddle1_matrix = glm::translate(g_paddle1_matrix, INIT_POS_PADDLE1);
+    g_paddle1_matrix = glm::translate(g_paddle1_matrix, g_paddle1_position);
+    g_paddle2_matrix = glm::mat4(1.0f);
+    g_paddle2_matrix = glm::translate(g_paddle2_matrix, INIT_POS_PADDLE2);
+    g_paddle2_matrix = glm::translate(g_paddle2_matrix, g_paddle2_position);
+    g_ball_matrix = glm::mat4(1.0f);
+    g_ball_matrix = glm::translate(g_ball_matrix, INIT_POS_BALL);
+    g_ball_matrix = glm::translate(g_ball_matrix, g_ball_position);
+    // --- SCALING --- //
+    g_midline_matrix = glm::mat4(1.0f);
+    g_paddle1_matrix = glm::scale(g_paddle1_matrix, INIT_SCALE_PADDLE);
+    g_paddle2_matrix = glm::scale(g_paddle2_matrix, INIT_SCALE_PADDLE);
+    g_ball_matrix = glm::scale(g_ball_matrix, INIT_SCALE_BALL);
+    g_midline_matrix = glm::scale(g_midline_matrix, INIT_SCALE_MIDLINE);
+
+
+    // --- TERMINATE GAME IF BALL MISSED --- //
+    if (fabs(g_ball_position.x)-INIT_SCALE_BALL.x/2 > 5.0f){
+        g_app_status = TERMINATED;
+    }
 }
 
-
-void draw_object(glm::mat4& object_g_model_matrix, GLuint& object_texture_id)
+void draw_object(glm::mat4& object_model_matrix, GLuint& object_texture_id)
 {
-    g_shader_program.set_model_matrix(object_g_model_matrix);
+    g_shader_program.set_model_matrix(object_model_matrix);
     glBindTexture(GL_TEXTURE_2D, object_texture_id);
-    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so use 6, not 3
+    glDrawArrays(GL_TRIANGLES, 0, 6); // we are now drawing 2 triangles, so we use 6 instead of 3
 }
 
-
-void render()
-{
+void render() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     // Vertices
-    float vertices[] =
-    {
+    float vertices[] = {
         -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
         -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
     };
 
     // Textures
-    float texture_coordinates[] =
-    {
+    float texture_coordinates[] = {
         0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
         0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
     };
 
-    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false,
-        0, vertices);
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false, 0, vertices);
     glEnableVertexAttribArray(g_shader_program.get_position_attribute());
 
-    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT,
-        false, 0, texture_coordinates);
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT, false, 0, texture_coordinates);
     glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
     // Bind texture
-    draw_object(g_ryu_matrix, g_ryu_texture_id);
-    draw_object(g_ken_matrix, g_ken_texture_id);
+    draw_object(g_paddle1_matrix, g_paddle1_texture_id);
+    draw_object(g_paddle2_matrix, g_paddle2_texture_id);
+    draw_object(g_midline_matrix, g_midline_texture_id);
+    draw_object(g_ball_matrix, g_ball_texture_id);
+    
+
 
     // We disable two attribute arrays now
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
@@ -252,7 +389,6 @@ void render()
 
     SDL_GL_SwapWindow(g_display_window);
 }
-
 
 void shutdown() { SDL_Quit(); }
 
